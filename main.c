c6232d047b05e98d1082d4f036987f6d1029ba28
MercyNJ <mercinjuguna@gmail.com>
2023-04-17 02:57:57 -0700

    The main program file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - The entry point of our program.
+ * @argc: The argument count.
+ * @argv: The arguments vector.
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
