5bf1989c400908daec7559e55576979379968c64
Kipkoech99 <kipkoechevans99@gmail.com>
2023-04-28 16:10:45 +0300

    Commit 2 to simple_shell group project

---
+++ errors.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ *_eputs - prints user input
+ * @str: the input to be printed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - prints char c to stderr
+ * @c: The character to display
+ *
+ * Return: 1 (success), -1 on error.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - displays char c to given fd
+ * @c: The character to display
+ * @fd: file descriptor
+ *
+ * Return: 1(success), -1 on error.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - displays input
+ * @str: input to be displayed
+ * @fd:file descriptor
+ *
+ * Return: number of characters
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
